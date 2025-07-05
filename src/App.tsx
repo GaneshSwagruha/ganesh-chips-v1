b11a686d762e0a9f8160b5bbccb7538ba96a731f
GaneshSwagruha <Ganeshswagruhafoods@gmail.com>
2025-07-05 11:18:08 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -61,8 +61,8 @@ function App() {
       <main className="flex-grow" style={{ minHeight: "100vh" }}>
         <Suspense fallback={<LoadingSpinner />}>
           <Routes>
-{/*             <Route path="/" element={<HomePage />} />
-            <Route path="/products" element={<ProductsPage />} /> */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="/products" element={<ProductsPage />} />
             <Route path="/visit" element={<VisitPage />} />
             <Route path="/contact" element={<ContactPage />} />
           </Routes>
